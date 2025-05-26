61fe38739982e8dd941d5cf60bd5488a85dda260
Xie Yuheng <xyheme@gmail.com>
2025-05-27 01:35:12 +0800

    up

--- src/AST.rkt
+++ src/AST.rkt
@@ -3,7 +3,9 @@
 (require "utils.rkt")
 
 (provide (struct-out Int)
-         (struct-out Prim))
+         (struct-out Prim)
+         (struct-out Program))
 
 (define-data Int [value])
 (define-data Prim [op args])
+(define-data Program [info body])
--- src/main.rkt
+++ src/main.rkt
@@ -1,10 +1,14 @@
 #lang racket
 
+(require racket/fixnum)
+
 (require "utils.rkt")
 (require "AST.rkt")
 
 (display
  (list (Int 1)
+       (Prim '- (list (Int 8)))
        (Int? (Int 1))
        (Int-value (Int 1))
+       (fx+ 1 2)
        (assert (equal? (Int 1) (Int 1)))))
