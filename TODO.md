270c7213bcc43cf0857e4b1f2991ae424f460c8f
Xie Yuheng <xyheme@gmail.com>
2025-06-04 22:50:18 +0800

    [pass] 040-select-instructions -- fix test by format-x86-program

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 > 2 Integers and Variables
 
-[pass] 040-select-instructions -- fix test by format-x86-program
-
 [pass] 050-assign-homes
 [pass] 060-patch-instructions
 [pass] 070-prelude-and-conclusion
--- src/passes/040-select-instructions.test.rkt
+++ src/passes/040-select-instructions.test.rkt
@@ -18,28 +18,28 @@
                    (+ x (let ([x 22]) x)))])
           y))))))))
 
-; format-x86-program
-(select-instructions
- (explicate-control
-  (rco-program
-   (uniquify
-    (parse-program
-     '(program
-       ()
-       (let ([y (let ([x.1 20])
-                  (let ([x.2 22])
-                    (+ x.1 x.2)))])
-         y)))))))
+(format-x86-program
+ (select-instructions
+  (explicate-control
+   (rco-program
+    (uniquify
+     (parse-program
+      '(program
+        ()
+        (let ([y (let ([x.1 20])
+                   (let ([x.2 22])
+                     (+ x.1 x.2)))])
+          y))))))))
 
-;; format-x86-program
-(select-instructions
- (explicate-control
-  (rco-program
-   (uniquify
-    (parse-program
-     '(program
-       ()
-       (let ([z (let ([y (let ([x 6])
-                           x)])
-                  y)])
-         z)))))))
+(format-x86-program
+ (select-instructions
+  (explicate-control
+   (rco-program
+    (uniquify
+     (parse-program
+      '(program
+        ()
+        (let ([z (let ([y (let ([x 6])
+                            x)])
+                   y)])
+          z))))))))
